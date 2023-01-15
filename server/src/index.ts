18fd0a63af1dd4518a3b3c0b13d71e42fe434306
nbx7 <101077340+nbx7@users.noreply.github.com>
2023-01-15 12:14:47 -0300

    set server fold structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,3 +6,8 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
