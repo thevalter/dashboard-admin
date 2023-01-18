1736fe2a1c08a04beb03f7a97fdf5654172da077
nbx7 <101077340+nbx7@users.noreply.github.com>
2023-01-18 10:05:08 -0300

    add theme file

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,32 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import clientRoutes from './routes/client';
+import generalRoutes from './routes/general';
+import managementRoutes from './routes/management';
+import salesRoutes from './routes/sales';
+
+
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
+
+mongoose.set('strictQuery', true);
+
+const PORT = process.env.PORT || 3000;
+mongoose.connect(`${process.env.DB_USER}`)
+.then(() => {
+    app.listen(PORT, () => console.log(`App connected on PORT ${PORT}`))
+})
+.catch((error) => console.log(`${error} did not connect`))
